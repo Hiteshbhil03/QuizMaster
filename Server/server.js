ae41e7c86d5dc44a062e3a6ee907bbc04abfc3af
Hanu Singh <97401096+iknowaditya@users.noreply.github.com>
2024-10-15 15:49:05 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ connectDB().catch((err) => console.log(err));
 
 app.use(cors({
    // origin: process.env.CORS_ORIGIN || 'http://localhost:5173'
-   origin: "https://quiz-app-full-stack-front-otl2mt4z2-iknowadityas-projects.vercel.app"
+   origin: "https://quiz-app-full-stack-frontend.onrender.com"
 }));
 
 app.use(express.json());
